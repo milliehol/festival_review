82cd841a59fb12e907d15ddeeb15583d793636b2
milliehol <carla.programmer@gmail.com>
2020-05-16 13:12:09 -0400

    added relationships to user file.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+	has_secure_password
+  has_many :reviews
+  has_many :reviewed_festivals, :through => :reviews, source: :festival
+  has_many :festivals
+  validates :username, uniqueness: true, presence: true
+  validates :name, uniqueness: true, presence: true
 end
