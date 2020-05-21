11de707c7e7b832056b2f839a17d145e70e30ed9
milliehol <carla.programmer@gmail.com>
2020-05-20 20:56:48 -0400

    updated festivals controller.

--- app/controllers/festivals_controller.rb
+++ app/controllers/festivals_controller.rb
@@ -1,4 +1,7 @@
 class FestivalsController < ApplicationController
+  before_action :set_festival, only:[:show, :edit, :update]
+  before_action :redirect_if_not_logged_in
+
   def index
     @festivals = Festival.order_by_rating.includes(:city)
   end
@@ -14,6 +17,7 @@ class FestivalsController < ApplicationController
     if @festival.save
       redirect_to festival_path(@festival)
     else
+      @festival.build_city
       render :new
     end
   end
@@ -22,9 +26,25 @@ class FestivalsController < ApplicationController
     @festivals = Festival.all
   end
 
+  def update
+    if @festival.update(festival_params)
+      redirect_to festival_path(@festival)
+    else
+      render :edit
+    end
+  end
+
+  def edit
+  end
+
   private
 
   def festival_params
     params.require(:festival).permit(:name, :city, :city_id, city_attributes: [:name])
+  end
+
+  def set_festival
+    @festival = Festival.find_by(params[:id])
+    redirect_to festivals_path if !@festival
   end
 end
