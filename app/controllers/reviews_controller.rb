f83abfa67bb85980c97627519208a407c4f6aca1
milliehol <carla.programmer@gmail.com>
2020-05-20 20:52:00 -0400

    updated reviews controller and helper.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,5 @@
 class ReviewsController < ApplicationController
-  #before_action :redirect_if_not_logged_in
+  before_action :redirect_if_not_logged_in
 
   def new
     #need festival_id b/c of nested route
--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,2 +1,11 @@
 module ReviewsHelper
+
+  def display_header(review)
+    if params[:festival_id]
+      content_tag(:h1, "Add a Review for #{review.festival.name} -  #{review.city.name}")
+    else
+      content_tag(:h1, "Write a review")
+    end
+  end
+
 end
