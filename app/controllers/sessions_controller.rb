251ffde11cf72d8dcf1b9a9ad23074dc9d9aeb64
milliehol <carla.programmer@gmail.com>
2020-05-22 15:27:39 -0400

    updated sessions controller for facebook login.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,6 +9,14 @@ class SessionsController < ApplicationController
   end
 
   def create
+
+    if params[:provider] == 'facebook_auth_code'
+    @user = User.find_or_create_by(uid: auth['uid']) do |u|
+      u.name = auth['info']['name']
+      u.email = auth['info']['email']
+      u.image = auth['info']['image']
+      end
+    else
       @user = User.find_by(username: params[:user][:username])
       #did we find someone & did they put in the right password?
       if @user.try(:authenticate, params[:user][:password])
@@ -17,10 +25,17 @@ class SessionsController < ApplicationController
       else
       redirect_to login_path
       end
+    end
   end
 
   def destroy
     session.delete("user_id")
     redirect_to root_path
+  end
+
+  private
+
+  def auth
+    request.env['omniauth.auth']
   end
 end
