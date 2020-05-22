b60fa37747c442c356756bded21af3b1e02077b8
milliehol <carla.programmer@gmail.com>
2020-05-22 14:52:07 -0400

    updated user to contain email instead of username.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(
       :name,
-      :username,
+      :email,
       :password
     )
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   has_many :reviews
   has_many :reviewed_festivals, :through => :reviews, source: :festival
   has_many :festivals
-  validates :username, uniqueness: true, presence: true
+  validates :email, uniqueness: true, presence: true
   validates :name, uniqueness: true, presence: true
 end
