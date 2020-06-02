c5088419363ed76a761573bee6401289769e9421
milliehol <carla.programmer@gmail.com>
2020-06-01 22:09:58 -0400

    updated application helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,3 @@
 module ApplicationHelper
-  def render_nav_bar
-    if verified_user
-      render partial: 'layouts/loggedin_links'
-    else
-      render partial: 'layouts/loggedout_links'
-    end
-  end
+
 end
