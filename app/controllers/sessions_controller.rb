13fd7390da4ee89550377240169caa0ac823f008
milliehol <carla.programmer@gmail.com>
2020-06-01 22:12:26 -0400

    updated sessions controller.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
   skip_before_action :verified_user, only: [:new, :create, :welcome]
   
-   def welcome
+  def welcome
     @user = User.new
   end
 
@@ -15,26 +15,12 @@ class SessionsController < ApplicationController
       session[:user_id] = @user.id
       redirect_to user_path(@user)
 
-
-      #params[:provider] == 'github'
-      #@user = User.create_or_find_by(uid: auth['uid']) do |u|
-      # u.uid = auth['uid']
-      # u.username = auth['info']["username"]
-      # u.email = auth['info']["email"]
-      # u.password = SecureRandom.hex
-      #end
-
     elsif  @user = User.find_by(username: params[:user][:username])
       #did we find someone & did they put in the right password?
       @user.try(:authenticate, params[:user][:password])
       session[:user_id] = @user.id
       redirect_to user_path(@user)
-    #if params[:provider] == 'facebook_auth_code'
-    #@user = User.find_or_create_by(uid: auth['uid']) do |u|
-    #u.name = auth['info']['name']
-    #u.email = auth['info']['email']
-    #u.image = auth['info']['image']
-    #end
+      
     else
 
       redirect_to login_path
